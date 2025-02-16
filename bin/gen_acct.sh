f84716ad5622c712cf379b488bf7e942cc659257
Landon Curt Noll <github-mail@asthe.com>
2025-02-16 14:03:10 -0800

    add bin/gen_acct.sh tool
    The `bin/gen_acct.sh` tool will generate an IOCCC submit server account.

---
+++ bin/gen_acct.sh
@@ -0,0 +1,246 @@
+#!/usr/bin/env bash
+#
+# gen_acct.sh - generate an IOCCC submit server account
+#
+# Given an email address of someone who has registered the IOCCC,
+# generate an IOCCC submit server account and output information
+# about the newly created IOCCC submit server in stdout.
+#
+# Copyright (c) 2025 by Landon Curt Noll.  All Rights Reserved.
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright, this permission notice and text
+# this comment, and the disclaimer below appear in all of the following:
+#
+#       supporting documentation
+#       source copies
+#       source works derived from this source
+#       binaries derived from this source or from derived source
+#
+# LANDON CURT NOLL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
+# EVENT SHALL LANDON CURT NOLL BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+# USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+# chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
+#
+# Share and enjoy! :-)
+
+
+# firewall - run only with a bash that is version 5.1.8 or later
+#
+# The "/usr/bin/env bash" command must result in using a bash that
+# is version 5.1.8 or later.
+#
+# We could relax this version and insist on version 4.2 or later.  Versions
+# of bash between 4.2 and 5.1.7 might work.  However, to be safe, we will require
+# bash version 5.1.8 or later.
+#
+# WHY 5.1.8 and not 4.2?  This safely is done because macOS Homebrew bash we
+# often use is "version 5.2.26(1)-release" or later, and the RHEL Linux bash we
+# use often use is "version 5.1.8(1)-release" or later.  These versions are what
+# we initially tested.  We recommend you either upgrade bash or install a newer
+# version of bash and adjust your $PATH so that "/usr/bin/env bash" finds a bash
+# that is version 5.1.8 or later.
+#
+# NOTE: The macOS shipped, as of 2024 March 15, a version of bash is something like
+#       bash "version 3.2.57(1)-release".  That macOS shipped version of bash
+#       will NOT work.  For users of macOS we recommend you install Homebrew,
+#       (see https://brew.sh), and then run "brew install bash" which will
+#       typically install it into /opt/homebrew/bin/bash, and then arrange your $PATH
+#       so that "/usr/bin/env bash" finds "/opt/homebrew/bin" (or whatever the
+#       Homebrew bash is).
+#
+# NOTE: And while MacPorts might work, we noticed a number of subtle differences
+#       with some of their ported tools to suggest you might be better off
+#       with installing Homebrew (see https://brew.sh).  No disrespect is intended
+#       to the MacPorts team as they do a commendable job.  Nevertheless we ran
+#       into enough differences with MacPorts environments to suggest you
+#       might find a better experience with this tool under Homebrew instead.
+#
+if [[ -z ${BASH_VERSINFO[0]} ||
+         ${BASH_VERSINFO[0]} -lt 5 ||
+         ${BASH_VERSINFO[0]} -eq 5 && ${BASH_VERSINFO[1]} -lt 1 ||
+         ${BASH_VERSINFO[0]} -eq 5 && ${BASH_VERSINFO[1]} -eq 1 && ${BASH_VERSINFO[2]} -lt 8 ]]; then
+    echo "$0: ERROR: bash version needs to be >= 5.1.8: $BASH_VERSION" 1>&2
+    echo "$0: Warning: bash version >= 4.2 might work but 5.1.8 was the minimum we tested" 1>&2
+    echo "$0: Notice: For macOS users: install Homebrew (see https://brew.sh), then run" \
+         ""brew install bash" and then modify your \$PATH so that \"#!/usr/bin/env bash\"" \
+         "finds the Homebrew installed (usually /opt/homebrew/bin/bash) version of bash" 1>&2
+    exit 4
+fi
+
+# setup bash file matching
+#
+# We must declare arrays with -ag or -Ag, and we need loops to "export" modified variables.
+# This requires a bash with a version 4.2 or later.  See the larger comment above about bash versions.
+#
+shopt -s nullglob       # enable expanded to nothing rather than remaining unexpanded
+shopt -u failglob       # disable error message if no matches are found
+shopt -u dotglob        # disable matching files starting with .
+shopt -u nocaseglob     # disable strict case matching
+shopt -u extglob        # enable extended globbing patterns
+shopt -s globstar       # enable ** to match all files and zero or more directories and subdirectories
+
+
+# setup
+#
+export VERSION="1.0.0 2025-02-16"
+NAME=$(basename "$0")
+export NAME
+export V_FLAG=0
+export TOPDIR="/var/ioccc"
+if [[ ! -d $TOPDIR ]]; then
+    # not on submit server, assume testing in .
+    TOPDIR="."
+fi
+IOCCC_PASSWD_PY=$(type -P ioccc_passwd.py)
+export IOCCC_PASSWD_PY
+if [[ -z $IOCCC_PASSWD_PY ]]; then
+    IOCCC_PASSWD_PY="./bin/ioccc_passwd.py"
+fi
+#
+export NOOP=
+export DO_NOT_PROCESS=
+
+
+# usage
+#
+export USAGE="usage: $0 [-h] [-v level] [-V] [-n] [-N] [-t topdir] [-p pwtool] email
+
+	-h		print help message and exit
+	-v level	set verbosity level (def level: 0)
+	-V		print version string and exit
+
+	-n		go thru the actions, but do not update any files (def: do the action)
+	-N		do not process anything, just parse arguments (def: process something)
+
+	-t appdir	app directory path (def: $TOPDIR)
+
+	-p pwtool	tool to create an IOCCC submit server account (def: $IOCCC_PASSWD_PY)
+
+	email		email address that we registed with the IOCCC
+
+Exit codes:
+     0         all OK
+     1	       failed to generate an IOCCC submit server account
+     2         -h and help string printed or -V and version string printed
+     3         command line error
+     4	       topdir is not a directory
+ >= 10         internal error
+
+$NAME version: $VERSION"
+
+
+# parse command line
+#
+while getopts :hv:VnNt:p: flag; do
+  case "$flag" in
+    h) echo "$USAGE" 1>&2
+	exit 2
+	;;
+    v) V_FLAG="$OPTARG"
+	;;
+    V) echo "$VERSION"
+	exit 2
+	;;
+    n) NOOP="-n"
+	;;
+    N) DO_NOT_PROCESS="-N"
+	;;
+    t) TOPDIR="$OPTARG"
+	;;
+    p) IOCCC_PASSWD_PY="$OPTARG"
+	;;
+    \?) echo "$0: ERROR: invalid option: -$OPTARG" 1>&2
+	echo 1>&2
+	echo "$USAGE" 1>&2
+	exit 3
+	;;
+    :) echo "$0: ERROR: option -$OPTARG requires an argument" 1>&2
+	echo 1>&2
+	echo "$USAGE" 1>&2
+	exit 3
+	;;
+    *) echo "$0: ERROR: unexpected value from getopts: $flag" 1>&2
+	echo 1>&2
+	echo "$USAGE" 1>&2
+	exit 3
+	;;
+  esac
+done
+#
+# remove the options
+#
+shift $(( OPTIND - 1 ));
+#
+if [[ $V_FLAG -ge 5 ]]; then
+    echo "$0: debug[5]: file argument count: $#" 1>&2
+fi
+if [[ $# -ne 1 ]]; then
+    echo "$0: ERROR: expected 1 arg, found: $#" 1>&2
+    exit 3
+fi
+export EMAIL="$1"
+if [[ -z $EMAIL ]]; then
+    echo "$0: ERROR: email arg is empty" 1>&2
+    exit 3
+fi
+
+
+# print running info if verbose
+#
+# If -v 3 or higher, print exported variables in order that they were exported.
+#
+if [[ $V_FLAG -ge 3 ]]; then
+    echo "$0: debug[3]: VERSION=$VERSION" 1>&2
+    echo "$0: debug[3]: NAME=$NAME" 1>&2
+    echo "$0: debug[3]: V_FLAG=$V_FLAG" 1>&2
+    echo "$0: debug[3]: TOPDIR=$TOPDIR" 1>&2
+    echo "$0: debug[3]: IOCCC_PASSWD_PY=$IOCCC_PASSWD_PY" 1>&2
+    echo "$0: debug[3]: NOOP=$NOOP" 1>&2
+    echo "$0: debug[3]: DO_NOT_PROCESS=$DO_NOT_PROCESS" 1>&2
+    echo "$0: debug[3]: EMAIL=$EMAIL" 1>&2
+fi
+
+
+# move to the top of the tree
+#
+export CD_FAILED=""
+cd "$TOPDIR" || CD_FAILED="true"
+if [[ -n $CD_FAILED ]]; then
+    echo "$0: ERROR: cd $TOPDIR failed" 1>&2
+    exit 4
+fi
+
+
+# -N stops early before any processing is performed
+#
+if [[ -n $DO_NOT_PROCESS ]]; then
+    if [[ $V_FLAG -ge 3 ]]; then
+	echo "$0: debug[3]: arguments parsed, -N given, exiting 0" 1>&2
+    fi
+    exit 0
+fi
+
+
+# generate an IOCCC submit server account
+#
+if [[ $V_FLAG -ge 1 ]]; then
+    echo "$0: debug[1]: about to run: $IOCCC_PASSWD_PY -U -E -c -e $EMAIL" 1>&1
+fi
+"$IOCCC_PASSWD_PY" -U -E -c -e "$EMAIL"
+status="$?"
+if [[ $status -ne 0 ]]; then
+    echo "$0: ERROR: $IOCCC_PASSWD_PY -U -E -c -e $EMAIL failed, error: $status" 1>&2
+    exit 1
+fi
+
+
+# All Done!!! All Done!!! -- Jessica Noll, Age 2
+#
+exit 0
